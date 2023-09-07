2945f6b9dfcb0207fab83a310eaf5258e1aa172e
Aayam <aayam.maharjan3@gmail.com>
2023-09-07 22:26:18 +0545

    complition of plants and changes indashboard page UI
    Added a section to edit the plants in dashboard
    
    created UI for plants page containg filter section and plants to buy
    
    changes into footer and header and adding links

--- Botanic_Garden/static/JS/script.js
+++ Botanic_Garden/static/JS/script.js
@@ -10,3 +10,20 @@ function dropMenu(){
         dropMenu.style.display = "block"
     }
 }
+
+// Custom function to display and keep the value in a tooltip
+function updateValue() {
+    const valueSlider = document.getElementById('valueSlider');
+    const valuePopup = document.getElementById('valuePopup');
+    const value = valueSlider.value;
+    valuePopup.textContent = `Rs${value}`;
+    valuePopup.style.left = `${(value / 20000) * 100}%`;
+    console.log("message")
+}
+
+// Attach the custom function to the input event
+const valueSlider = document.getElementById('valueSlider');
+valueSlider.addEventListener('input', updateValue);
+
+// Display the tooltip initially when the page loads
+window.addEventListener('DOMContentLoaded', updateValue);
